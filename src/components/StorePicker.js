40bc6e759809486a44cface17145042ad5796ca2
root <root@PC-Timur.localdomain>
2019-01-10 21:54:30 +0100

    events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import { getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+    // Bind custom goToStore function to `this`.
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this)
+    // }
+   
+
+    // goToStore(event) {
+    //     console.log(this.myInput);
+
+    //     event.preventDefault();
+    // }
+
+    myInput = React.createRef();
+    // Another way to `bind` this to a function
+    goToStore = (event) => {
+        const storeName = this.myInput.current.value;
+        this.props.history.push(`/store/${storeName}`)
+        event.preventDefault();
+    }
     render() {
         return (
             <React.Fragment>
                 { /* This is how comments are in JSX */}
-                <form className="store-selector">
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>Please Enter a Store</h2>
-                    <input type="text" required placeholder="Store Name"/>
+                    <input 
+                        type="text"
+                        ref={this.myInput} 
+                        required 
+                        placeholder="Store Name" 
+                        defaultValue={getFunName()}
+                    />
                     <button type="submit">Visit Store</button>
                 </form>
             </React.Fragment>
